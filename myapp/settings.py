176cdffd56b8b65baa2dd3d359ce114dd20c000b
Tejas Lamkhade <105742352+Tejas-24ytj@users.noreply.github.com>
2025-02-06 19:23:01 +0530

    Fixed Secret Leaks

--- myapp/settings.py
+++ myapp/settings.py
@@ -86,10 +86,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         # 'NAME': BASE_DIR / 'db.sqlite3',
-        'NAME': 'database2', # add you database name (schema name eg: newemp)
-        'USER': 'admin',
-        'PASSWORD': 'G9h!kLm2',
-        'HOST': 'database-2.cf04c4c2avip.ap-southeast-1.rds.amazonaws.com',
+        'NAME': '', # add you database name (schema name eg: newemp)
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '3306'
     }
 }
